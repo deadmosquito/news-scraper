e2bc2a455c18804fb9893b0a37e335bf87d00ad8
Womp Rat Bullseye <stevensorensen13@gmail,com>
2019-10-23 13:10:11 -0700

    update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with axios
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("http://www.latimes.com/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
