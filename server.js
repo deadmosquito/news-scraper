94bf395994d8a2ad04d9623176f7b29b6554823f
Womp Rat Bullseye <stevensorensen13@gmail,com>
2019-10-23 13:32:53 -0700

    update server.js

--- server.js
+++ server.js
@@ -27,7 +27,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/mongoHeadline", { useNewUrlParser: true });
+
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
