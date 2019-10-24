f45c4e67f594719da805d318266792281eed0f79
Womp Rat Bullseye <stevensorensen13@gmail,com>
2019-10-24 13:47:09 -0700

    changed db mongo

--- server.js
+++ server.js
@@ -27,7 +27,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/mongoHeadline", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
 
 
 
