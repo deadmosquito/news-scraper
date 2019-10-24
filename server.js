9504f9075f89a93daafc41605e5235b36b9f5dd0
Womp Rat Bullseye <stevensorensen13@gmail,com>
2019-10-24 14:21:41 -0700

    update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 8000;
 
 // Initialize Express
 var app = express();
